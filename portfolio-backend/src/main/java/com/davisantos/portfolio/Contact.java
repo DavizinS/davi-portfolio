485bdbad8fcb8eead203a5f41284d6e85e7d8642
bibiisantanna <93940192+DavizinS@users.noreply.github.com>
2026-03-07 12:43:40 -0300

    up

---
+++ portfolio-backend/src/main/java/com/davisantos/portfolio/Contact.java
@@ -0,0 +1,39 @@
+package com.davisantos.portfolio;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "contatos")
+public class Contact {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false)
+    private String email;
+    
+    private String assunto;
+    
+    @Column(nullable = false, length = 600)
+    private String descricao;
+    
+    public Long getId() { return id; }
+    
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public String getAssunto() { return assunto; }
+    public void setAssunto(String assunto) { this.assunto = assunto; }
+    
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+    
+    
+}
--- portfolio-backend/src/main/java/com/davisantos/portfolio/service/ContactService.java
+++ portfolio-backend/src/main/java/com/davisantos/portfolio/service/ContactService.java
@@ -1,18 +1,38 @@
 package com.davisantos.portfolio.service;
 
+import com.davisantos.portfolio.Contact;
 import com.davisantos.portfolio.dto.ContactRequest;
+import com.davisantos.portfolio.repository.ContactRepository;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class ContactService {
+    
+    private final ContactRepository contactRepository;
+    
+    public ContactService(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+    
+    
     public void process(ContactRequest req) {
-        System.out.println("===============");
+        
+        //entidades
+        Contact contact = new Contact();
+        contact.setName(req.getName());
+        contact.setEmail(req.getEmail());
+        contact.setAssunto(req.getAssunto());
+        contact.setDescricao(req.getDescricao());
+        
+        contactRepository.save(contact);
+        
+        // ---- LOG ---- //
+        System.out.println("====== LEAD NOVO  ========");
         System.out.println("Nome:   " + req.getName());
         System.out.println("Email:   " + req.getEmail());
         System.out.println("Assunto:   " + req.getAssunto());
-        System.out.println("Texto:   " + req.getDescricao());
-        System.out.println("================");
+        System.out.println("===========================");
         
         
     }
